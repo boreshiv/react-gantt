188ceed9579cf8c7eb60b41e708675bb5f80f6dd
boreshiv <boreshiv@gmail.com>
2024-01-17 01:26:09 +0530

    ON MOVE SCALER IMPLEMENTED

--- src/components/Gantt/GanttChart.js
+++ src/components/Gantt/GanttChart.js
@@ -11,12 +11,11 @@ const GanttChart = (props, ref) => {
   const {tableDefs:{columnData=[]}} = props // table column data
   const chartRef = useRef(ref);
   const dataRef = useRef(null);
-
   const chartAreaWidth = useMemo(() => {
     const dayDiff = (timeLineEnd - timeLineStart) / 1000 / 60 / 60 / 24
     return dayDiff * config.scaleWidth
   }, [timeLineStart, timeLineEnd])
-
+  const scalerRef = useRef(null)
   const handleDataScroll =(e) => {
     if (chartRef.current) {
       chartRef.current.scrollTop = e.target.scrollTop;
@@ -28,7 +27,11 @@ const GanttChart = (props, ref) => {
       dataRef.current.scrollTop = e.target.scrollTop;
     }
   }
-
+  const guidleLineLeft = useMemo(() => {
+    const today = new Date()
+    const todayDiff = (today - timeLineStart) / 1000 / 60 / 60 / 24
+    return todayDiff * config.scaleWidth
+  }, [timeLineStart])
   return (
     <div className="gantt-container">
       <div ref={dataRef} className="staff-table" onScroll={handleDataScroll}>
@@ -45,8 +48,41 @@ const GanttChart = (props, ref) => {
           {/* Render the timeline here */}
           <Timeline start={timeLineStart} end={timeLineEnd} chartAreaWidth={chartAreaWidth} timelineScale={timelineScale} />
         </div>
-        <div className="chart-area" style={{ width: chartAreaWidth + "px" }} >
+        <div onMouseMove={(e)=>{
+
+          const left = e.target.getBoundingClientRect().left
+          if(scalerRef.current){
+            scalerRef.current.style.left = e.clientX - left  + "px"
+            scalerRef.current.style.title = e.clientX - left  + "px"
+            const date = new Date(timeLineStart) 
+            const dayDiff = (e.clientX - left) / config.scaleWidth
+            date.setDate(date.getDate() + dayDiff)
+            const hourDiff = (e.clientX - left) / config.scaleWidth * 24
+            date.setHours(hourDiff)
+            scalerRef.current.querySelector('.chart-area-scaler-title').innerHTML = date.toLocaleDateString() + " " + date.toLocaleTimeString()
+          }
+        }}
+        onKeyDown={(e)=>{
+          document.addEventListener('keydown', function (event) {
+            // Check if the left arrow key is pressed
+            if (event.key === 'ArrowLeft') {
+              // Move the element to the left by a certain amount (e.g., 10 pixels)
+              scalerRef.current.style.left = (parseFloat(scalerRef.current.style.left) - 1) + 'px';
+            }
+            // Check if the right arrow key is pressed
+            else if (event.key === 'ArrowRight') {
+              // Move the element to the right by a certain amount (e.g., 10 pixels)
+              scalerRef.current.style.left = (parseFloat(scalerRef.current.style.left) + 1) + 'px';
+            }
+          });
+        }}
+         className="chart-area" style={{ width: chartAreaWidth + "px" }} >
+            
           {/* Right chart area for tasks */}
+          <div className="chart-area-guideline" style={{left: guidleLineLeft + "px",height: staffs.length * config.rowHeight }}></div>
+          <div ref={scalerRef} className='chart-area-scaler' style={{ height: staffs.length * config.rowHeight }}>
+            <div className='chart-area-scaler-title'></div>
+          </div>
           {tasks.map((task) => (
             <Task
               onTaskClick={onTaskClick}
